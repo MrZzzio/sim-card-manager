2be82d9ce3a228ab1c1657dc240b9ed328b9f234
dmitry <zzzio@mail.ru>
2018-02-12 09:21:52 +0400

    fix }

--- src/app/card.service.ts
+++ src/app/card.service.ts
@@ -82,10 +82,11 @@ export class CardService {
  * @param operation - name of the operation that failed
  * @param result - optional value to return as the observable result
  */
-private handleError<T> (operation = 'operation', result?: T) {
-  return (error: any): Observable<T> => {
+ private handleError<T>(operation = 'operation', result ?: T) {
+   return (error: any): Observable<T> => {
     console.error(error);
-    this.log(`${operation} failed: ${error.message}`);
-    return of(result as T);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    }
   }
 }
