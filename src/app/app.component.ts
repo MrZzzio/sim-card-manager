43b8c6ce044cddd4a1c6fbbcb230ea31e8b3d3c5
dmitry <zzzio@mail.ru>
2018-02-20 11:56:12 +0400

    hide navigation while login & register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Sim-card Manager';
+
+  hasToken(): boolean {
+    return localStorage.getItem("x-auth-token");
+  }
 }
