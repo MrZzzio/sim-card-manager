8ada6a77688170ae71850efb1974f0f79dd7abdc
dmitry <zzzio@mail.ru>
2018-02-26 12:12:14 +0400

    pagination logic

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -3,6 +3,8 @@ import { Card } from '../card';
 import { CardService } from '../card.service';
 import { ActivatedRoute } from '@angular/router';
 
+import * as _ from 'lodash';
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
@@ -10,7 +12,9 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CardComponent implements OnInit {
 
-  cards: Card[];
+  private cards: Card[];
+
+  private pages: number[];
 
   constructor(private cardService: CardService,
               private route: ActivatedRoute) { }
@@ -22,7 +26,10 @@ export class CardComponent implements OnInit {
 
   getCards(page: number): void {
     this.cardService.getCards(page)
-      .subscribe(cards => this.cards = cards.content);
+      .subscribe(cards => {
+        this.cards = cards.content;
+        this.getPages(cards);
+      });
   }
 
   delete(card: Card): void {
@@ -30,4 +37,26 @@ export class CardComponent implements OnInit {
     this.cardService.deleteCard(card).subscribe();
   }
 
+  private getPages(cards: any): void {
+    let totalPages = cards.totalPages;
+    let currentPage = cards.number + 1;
+    let pageSize = cards.numberOfElements;
+    let startPage: number, endPage: number;
+    if (totalPages <= 10) {
+      startPage = 1;
+      endPage = totalPages;
+    } else {
+      if (currentPage <= 6) {
+        startPage = 1;
+        endPage = 10;
+      } else if (currentPage + 5 >= totalPages) {
+        startPage = totalPages - 9;
+        endPage = totalPages;
+      } else {
+        startPage = currentPage - 5;
+        endPage = currentPage + 4;
+      }
+    }
+    this.pages = _.range(startPage, endPage + 1);
+  }
 }
